67713fc089a918b4f958d7bb70c2764098d1adbb
shernk <31926953+shernk@users.noreply.github.com>
2020-12-29 22:16:53 +0700

    Fetching API from .env get error

--- client/src/API.js
+++ client/src/API.js
@@ -1,12 +1,12 @@
-const API_URL = "http://localhost:1337";
+// const API_URL = "http://localhost:1337";
 
 export async function listLogEntries() {
-  const response = await fetch(`${API_URL}/api/routes`);
+  const response = await fetch(`${process.env.API_URL}/api/routes`);
   return response.json();
 }
 
 export async function createLogEntry(entry) {
-  const response = await fetch(`${API_URL}/api/routes`, {
+  const response = await fetch(`${process.env.API_URL}/api/routes`, {
     method: "POST",
     headers: {
       "content-type": "application/json",
